51977170aa5842685d967eafac2aa7b8d2bf0a78
hbiegacz <biegacz.hanna@gmail.com>
2026-02-05 10:54:20 +0100

    switched to a lombok constructor

--- backend/src/main/java/com/bd2_team6/biteright/service/LimitHistoryService.java
+++ backend/src/main/java/com/bd2_team6/biteright/service/LimitHistoryService.java
@@ -1,25 +1,19 @@
 package com.bd2_team6.biteright.service;
 
 import com.bd2_team6.biteright.entities.limit_history.LimitHistory;
-import com.bd2_team6.biteright.entities.limit_history.LimitHistoryRepository;
 import com.bd2_team6.biteright.entities.user.User;
 import com.bd2_team6.biteright.entities.user.UserRepository;
 
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class LimitHistoryService {
     private final UserRepository userRepository;
-    private final LimitHistoryRepository limitHistoryRepository; // TODO: remove this
-
-    @Autowired
-    public LimitHistoryService(UserRepository userRepository, LimitHistoryRepository limitHistoryRepository) {
-        this.userRepository = userRepository;
-        this.limitHistoryRepository = limitHistoryRepository;
-    }
 
     public Set<LimitHistory> findLimitHistoryByUsername(String username) {
         User user = userRepository.findByUsername(username)
